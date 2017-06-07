b3f0146558ef4f4dfa5a622acf000d6abd46e13e
akritikts <akritikts@gmail.com>
2017-06-08 01:04:52 +0530

    permissions added

--- app/src/main/java/silive/in/comedytv/activities/MainActivity.java
+++ app/src/main/java/silive/in/comedytv/activities/MainActivity.java
@@ -13,6 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import silive.in.comedytv.R;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
